964f9577f25f365e5aec8ba8af06f17f62eee6ed
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2021-11-27 18:17:31 +0100

    Writing to disk only on change

--- dccm.go
+++ dccm.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"docker-compose-manager/src/command"
 	dcm "docker-compose-manager/src/docker-compose-manager"
 	"fmt"
+	"github.com/btnguyen2k/consu/checksum"
 	"os"
 )
 
@@ -18,17 +20,22 @@ func main() {
 		fmt.Println(cError)
 		os.Exit(1)
 	}
+	cFile, _ := dcm.GetConfigFile()
+	cFileChecksum := checksum.Md5Checksum(cFile)
 
 	cmdErr := command.RootCommand.Execute()
 	if cmdErr != nil {
 		panic(cmdErr)
 	}
 
-	cFile, _ := dcm.GetConfigFile()
-	cWriteError := cFile.WriteToFile(configFilePath)
-	if cWriteError != nil {
-		fmt.Println(cWriteError)
-		os.Exit(1)
+	cFile, _ = dcm.GetConfigFile()
+	if bytes.Compare(checksum.Md5Checksum(cFile), cFileChecksum) != 0 {
+		// write to disk only when cFile struct has changed
+		cWriteError := cFile.WriteToFile(configFilePath)
+		if cWriteError != nil {
+			fmt.Println(cWriteError)
+			os.Exit(1)
+		}
 	}
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module docker-compose-manager
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1
+require (
+	github.com/btnguyen2k/consu/checksum v0.1.2
+	github.com/spf13/cobra v1.2.1
+)
