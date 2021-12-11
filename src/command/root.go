0ac43bc628570d079ddd0b614582b434d6117b2d
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2021-12-11 18:56:27 +0100

    Display help as default command

--- src/command/root.go
+++ src/command/root.go
@@ -2,11 +2,14 @@ package command
 
 import (
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var RootCommand = &cobra.Command{
 	Use:   "dccm",
 	Short: "Docker-composer CLI manager",
 	Run: func(cmd *cobra.Command, args []string) {
+		_ = cmd.Help()
+		os.Exit(0)
 	},
 }
